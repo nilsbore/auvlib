c7b78dc8642075e23ec217fc904ed1fc54e6635b
Nils Bore <nilsbore@gmail.com>
2018-05-03 11:08:46 +0200

    Got adding working, time to tune the process

--- src/visualize_process.cpp
+++ src/visualize_process.cpp
@@ -6,60 +6,39 @@
 #include <sparse_gp/sparse_gp.h>
 #include <sparse_gp/rbf_kernel.h>
 #include <sparse_gp/probit_noise.h>
+#include <sparse_gp/colormap.h>
 
 using namespace std;
 using PointT = pcl::PointXYZRGB;
-using submap_matrix = vector<vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> > >;
+using SubmapsT = vector<vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> > >;
+using ProcessT = sparse_gp<rbf_kernel, probit_noise>;
 
-const int colormap[][3] = {
-    { 166, 206, 227 },
-    { 31, 120, 180 },
-    { 178, 223, 138 },
-    { 51, 160, 44 },
-    { 251, 154, 153 },
-    { 227, 26, 28 },
-    { 253, 191, 111 },
-    { 255, 127, 0 },
-    { 202, 178, 214 },
-    { 106, 61, 154 },
-    { 255, 255, 153 },
-    { 177, 89, 40 },
-    { 141, 211, 199 },
-    { 255, 255, 179 },
-    { 190, 186, 218 },
-    { 251, 128, 114 },
-    { 128, 177, 211 },
-    { 253, 180, 98 },
-    { 179, 222, 105 },
-    { 252, 205, 229 },
-    { 217, 217, 217 },
-    { 188, 128, 189 },
-    { 204, 235, 197 },
-    { 255, 237, 111 },
-    { 255, 179, 0 },
-    { 128, 62, 117 },
-    { 255, 104, 0 },
-    { 166, 189, 215 },
-    { 193, 0, 32 },
-    { 206, 162, 98 },
-    { 0, 125, 52 },
-    { 246, 118, 142 },
-    { 0, 83, 138 },
-    { 255, 122, 92 },
-    { 83, 55, 122 },
-    { 255, 142, 0 },
-    { 179, 40, 81 },
-    { 244, 200, 0 },
-    { 127, 24, 13 },
-    { 147, 170, 0 },
-    { 89, 51, 21 },
-    { 241, 58, 19 },
-    { 35, 44, 22 }
-};
-
-submap_matrix read_submaps(const boost::filesystem::path& folder)
+Eigen::MatrixXd read_submap(const boost::filesystem::path& filename)
 {
-    submap_matrix submaps;
+	Eigen::MatrixXd points;
+	points.resize(90000, 3);
+	std::ifstream infile(filename.string());
+	std::string line;
+	int i = 0;
+	while (std::getline(infile, line)) {
+		std::istringstream iss(line);
+		double x, y, z;
+		if (!(iss >> x >> y >> z)) {
+			break;
+		} // error
+		if (i >= points.rows()) {
+			points.conservativeResize(points.rows() + 90000, 3);
+		}
+		points.row(i) << x, y, z;
+		++i;
+	}
+	points.conservativeResize(i, 3);
+	return points;
+}
+
+SubmapsT read_submaps(const boost::filesystem::path& folder)
+{
+    SubmapsT submaps;
 
 	bool should_break = false;
 	for (int ii = 0; !should_break; ++ii) {
@@ -77,26 +56,9 @@ submap_matrix read_submaps(const boost::filesystem::path& folder)
 				}
 				break;
 			}
-            Eigen::MatrixXd points;
-            points.resize(90000, 3);
-			std::ifstream infile(filename.string());
-			std::string line;
-	        int i = 0;
-			while (std::getline(infile, line)) {
-				std::istringstream iss(line);
-				double x, y, z;
-				if (!(iss >> x >> y >> z)) {
-					break;
-				} // error
-				if (i >= points.rows()) {
-                    points.conservativeResize(points.rows() + 90000, 3);
-				}
-				points.row(i) << x, y, z;
-			    ++i;
-			}
-			points.conservativeResize(i, 3);
+			Eigen::MatrixXd points = read_submap(filename);
 			if (jj == 0) {
-                submaps.push_back(vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> >);
+                submaps.push_back(vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> >());
             }
 			submaps.back().push_back(points);
 		}
@@ -105,7 +67,7 @@ submap_matrix read_submaps(const boost::filesystem::path& folder)
 	return submaps;
 }
 
-void visualize_submaps(submap_matrix& submaps)
+void visualize_submaps(SubmapsT& submaps)
 {
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
@@ -114,9 +76,9 @@ void visualize_submaps(submap_matrix& submaps)
         for (int jj = 0; jj < submaps[ii].size(); ++jj) {
 		    PointT p;
 			p.getVector3fMap() = submaps[ii][jj].row(i).cast<float>();
-		    p.r = colormap[counter % 44][0];
-		    p.g = colormap[counter % 44][1];
-		    p.b = colormap[counter % 44][2];
+		    p.r = colormap[i % 44][0];
+		    p.g = colormap[i % 44][1];
+		    p.b = colormap[i % 44][2];
 		    cloud->push_back(p);
             ++i;
 		}
@@ -132,32 +94,83 @@ void visualize_submaps(submap_matrix& submaps)
 	}
 }
 
-void train_gp(Eigen::MatrixXd& points)
+void train_gp(Eigen::MatrixXd& points, ProcessT& gp)
 {
-    using gp = sparse_gp<rbf_kernel, probit_noise>;
-	gp mygp(100, 0.1);
+    cout << "Training gaussian process..." << endl;
+
     Eigen::MatrixXd X = points.leftCols(2);
 	Eigen::VectorXd y = points.col(2);
 	gp.add_measurements(X, y);
 
-	MatrixXd X_star;
-    VectorXd f_star;
+    cout << "Done training gaussian process..." << endl;
+}
+
+void visualize_submap_and_gp(Eigen::MatrixXd& points, ProcessT& gp)
+{
+	double maxx = points.col(0).maxCoeff();
+	double minx = points.col(0).minCoeff();
+	double maxy = points.col(1).maxCoeff();
+	double miny = points.col(1).minCoeff();
+	double meanz = points.col(2).mean();
+
 	int sz = 50;
+	double xstep = (maxx - minx)/float(sz-1);
+	double ystep = (maxy - miny)/float(sz-1);
+    
+	cout << "Predicting gaussian process..." << endl;
+
+	MatrixXd X_star;
+    VectorXd f_star; // mean?
+	VectorXd V_star; // variance?
 	X_star.resize(sz*sz, 2);
-    int points = 0;
+    int i = 0;
     for (int y = 0; y < sz; ++y) { // ROOM FOR SPEEDUP
 	    for (int x = 0; x < sz; ++x) {
-		    ind = x*sz + y;
+		    //int ind = x*sz + y;
 		    /*if (!W(ind, i)) {
 			    continue;
 		    }*/
-		    X_star(points, 0) = res*((double(x) + 0.5f)/double(sz) - 0.5f);
-		    X_star(points, 1) = res*((double(y) + 0.5f)/double(sz) - 0.5f);
-		    ++points;
+		    X_star(i, 0) = minx + x*xstep;
+		    X_star(i, 1) = miny + y*ystep;
+		    ++i;
 	    }
     }
-    X_star.conservativeResize(points, 2);
-    mygp.predict_measurements(f_star, X_star, V_star);
+    X_star.conservativeResize(i, 2);
+    gp.predict_measurements(f_star, X_star, V_star);
+	
+	cout << "Done predicting gaussian process..." << endl;
+
+	Eigen::MatrixXd predicted_points(X_star.rows(), 3);
+	predicted_points.leftCols(3) = X_star;
+	predicted_points.col(2) = f_star;
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    for (int i = 0; i < predicted_points.rows(); ++i) {
+        PointT p;
+        p.getVector3fMap() = predicted_points.row(i).cast<float>();
+        p.r = colormap[0][0];
+	    p.g = colormap[0][1];
+		p.b = colormap[0][2];
+		cloud->push_back(p);
+    }
+    for (int i = 0; i < points.rows(); ++i) {
+        PointT p;
+        p.getVector3fMap() = points.row(i).cast<float>();
+        p.r = colormap[1][0];
+	    p.g = colormap[1][1];
+		p.b = colormap[1][2];
+		cloud->push_back(p);
+    }
+
+	cout << "Done constructing point cloud, starting viewer..." << endl;
+
+	//... populate cloud
+	pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+	viewer.showCloud (cloud);
+	while (!viewer.wasStopped ())
+	{
+	}
 }
 
 int main(int argc, char** argv)
@@ -165,8 +178,14 @@ int main(int argc, char** argv)
 	boost::filesystem::path folder(argv[1]);
 	cout << "Folder : " << folder << endl;
 
-	submap_matrix submaps = read_submaps(folder);
-	visualize_submaps(submaps);
+    //SubmapsT submaps = read_submaps(folder);
+	//visualize_submaps(submaps);
+	
+	Eigen::MatrixXd points = read_submap(folder / "patch_00_00.xyz");
+	
+	ProcessT gp(100, 100.);
+	train_gp(points, gp);
+	visualize_submap_and_gp(points, gp);
 
     return 0;
 }
