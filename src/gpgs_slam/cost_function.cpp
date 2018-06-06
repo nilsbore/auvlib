0f811c1cc150eb089e4c5957a5b0a7503b3fd22c
Nils Bore <nilsbore@gmail.com>
2018-06-06 14:47:00 +0200

    Got it working with proper jacobians

--- src/gpgs_slam/cost_function.cpp
+++ src/gpgs_slam/cost_function.cpp
@@ -46,8 +46,8 @@ GaussianProcessCostFunction::get_new_transform_jacobian(const Eigen::MatrixXd& X
     vector<Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d> > R1ds = euler_to_diff_matrices(rot1(0), rot1(1), rot1(2));
     vector<Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d> > R2ds = euler_to_diff_matrices(rot2(0), rot2(1), rot2(2));
     for (int i = 0; i < 3; ++i) {
-        R1s[i] = R1s[i].transpose();
-        R1ds[i] = R1ds[i].transpose();
+        R1s[i].transposeInPlace();
+        R1ds[i].transposeInPlace();
     }
     Eigen::Matrix3d R1 = R1s[2]*R1s[1]*R1s[0];
     Eigen::Matrix3d R2 = R2s[0]*R2s[1]*R2s[2];
@@ -60,16 +60,16 @@ GaussianProcessCostFunction::get_new_transform_jacobian(const Eigen::MatrixXd& X
         Eigen::MatrixXd J1(3, 6);
         Eigen::MatrixXd J2(3, 6);
 
-        J2.topRows<3>() = R1;
-        J1.topRows<3>() = -R1;
+        J2.leftCols<3>() = R1.transpose();
+        J1.leftCols<3>() = -R1.transpose();
 
-        J2.row(3) = (R1*R2ds[0]*R2s[1]*R2s[2]*p).transpose();
-        J2.row(4) = (R1*R2s[0]*R2ds[1]*R2s[2]*p).transpose();
-        J2.row(5) = (R1*R2s[0]*R2s[1]*R2ds[2]*p).transpose();
+        J2.col(3) = R1*R2ds[0]*R2s[1]*R2s[2]*p;
+        J2.col(4) = R1*R2s[0]*R2ds[1]*R2s[2]*p;
+        J2.col(5) = R1*R2s[0]*R2s[1]*R2ds[2]*p;
 
-        J1.row(3) = (R1s[2]*R1s[1]*R1ds[0]*(R2*p+t2-t1)).transpose();
-        J1.row(4) = (R1s[2]*R1ds[1]*R1s[0]*(R2*p+t2-t1)).transpose();
-        J1.row(5) = (R1ds[2]*R1s[1]*R1s[0]*(R2*p+t2-t1)).transpose();
+        J1.col(3) = R1s[2]*R1s[1]*R1ds[0]*(R2*p+t2-t1);
+        J1.col(4) = R1s[2]*R1ds[1]*R1s[0]*(R2*p+t2-t1);
+        J1.col(5) = R1ds[2]*R1s[1]*R1s[0]*(R2*p+t2-t1);
 
         J1s.push_back(J1);
         J2s.push_back(J2);
@@ -81,8 +81,10 @@ GaussianProcessCostFunction::get_new_transform_jacobian(const Eigen::MatrixXd& X
 bool GaussianProcessCostFunction::Evaluate(double const* const* parameters, double* residuals, double** jacobians) const
 {
     Eigen::Vector3d t1(parameters[0][0], parameters[0][1], parameters[0][2]);
+    Eigen::Vector3d rot1(parameters[1][0], parameters[1][1], parameters[1][2]);
     Eigen::Matrix3d R1 = euler_to_matrix(parameters[1][0], parameters[1][1], parameters[1][2]);
     Eigen::Vector3d t2(parameters[2][0], parameters[2][1], parameters[2][2]);
+    Eigen::Vector3d rot2(parameters[3][0], parameters[3][1], parameters[3][2]);
     Eigen::Matrix3d R2 = euler_to_matrix(parameters[3][0], parameters[3][1], parameters[3][2]);
 
     cout << "Translation: " << t1.transpose() << ", Rotation: \n" << R1 << endl;
@@ -106,37 +108,49 @@ bool GaussianProcessCostFunction::Evaluate(double const* const* parameters, doub
         //gp.compute_derivatives(dX, points.leftCols(2), points.col(2));
         //gp1.compute_neg_log_derivatives(dX, points2in1.leftCols(2), points2in1.col(2));
         cout << "Done computing derivatives..." << endl;
-        dX *= R1.transpose();
+        //dX *= R1.transpose();
+    
+        Eigen::MatrixXd points2sub = points2in1*R1.transpose()*R2;
+	    points2sub.rowwise() += (t1.transpose()*R2 - t2.transpose()*R2);
+    
+        vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> > J1s; 
+        vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> > J2s;
+        tie(J1s, J2s) = get_new_transform_jacobian(points2sub, t1, rot1, t2, rot2); 
 
-        Eigen::MatrixXd J(3, 6);
-        Eigen::MatrixXd deltas(points2in1.rows(), 6);
-        for (int m = 0; m < points2in1.rows(); ++m) {
-            get_transform_jacobian(J, R1*points2in1.row(m).transpose()+t1);
-            deltas.row(m) = dX.row(m)*J;
+        //Eigen::MatrixXd J(3, 6);
+        Eigen::MatrixXd delta1s(points2sub.rows(), 6);
+        Eigen::MatrixXd delta2s(points2sub.rows(), 6);
+        for (int m = 0; m < points2sub.rows(); ++m) {
+            //get_transform_jacobian(J, R1*points2in1.row(m).transpose()+t1);
+            delta1s.row(m) = dX.row(m)*J1s[m];
+            delta2s.row(m) = dX.row(m)*J2s[m];
         }
-        Eigen::RowVectorXd delta = deltas.colwise().mean();
+        Eigen::RowVectorXd delta1 = delta1s.colwise().mean();
+        Eigen::RowVectorXd delta2 = delta2s.colwise().mean();
         if (jacobians[0] != NULL) { 
             for (int j = 0; j < 3; ++j) {
-                jacobians[0][j] = -delta[j];
+                jacobians[0][j] = delta1[j];
             }
         }
         if (jacobians[1] != NULL) { 
             for (int j = 0; j < 3; ++j) {
-                jacobians[1][j] = -delta[3+j];
+                //jacobians[1][j] = 0.0000000000001*delta1[3+j];
+                jacobians[1][j] = delta1[3+j];
             }
         }
         if (jacobians[2] != NULL) { 
             for (int j = 0; j < 3; ++j) {
-                jacobians[2][j] = delta[j];
+                jacobians[2][j] = delta2[j];
             }
         }
         if (jacobians[3] != NULL) { 
             for (int j = 0; j < 3; ++j) {
-                jacobians[3][j] = delta[3+j];
+                //jacobians[3][j] = 0.0000000000001*delta2[3+j];
+                jacobians[3][j] = delta2[3+j];
             }
         }
 
-        cout << "Derivative: " << delta << endl;
+        cout << "Derivative: " << delta1 << ", " << delta2 << endl;
     }
     else {
         cout << "Not computing derivatives" << endl;
