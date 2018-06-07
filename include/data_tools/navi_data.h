2f15ea4c9b3abaab3114ea9032d42ec381287d9f
Nils Bore <nilsbore@gmail.com>
2018-06-07 14:41:43 +0200

    Added one library for all data structures

--- include/data_tools/navi_data.h
+++ include/data_tools/navi_data.h
@@ -1,10 +1,11 @@
 #ifndef NAVI_DATA_H
 #define NAVI_DATA_H
 
-#include <eigen_cereal/eigen_cereal.h>
 #include <Eigen/Dense>
 #include <boost/filesystem.hpp>
 #include <boost/range.hpp>
+
+#include <data_tools/data_structures.h>
 
 using TransT = std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> >;
 using RotsT = std::vector<Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d> >;
@@ -12,49 +13,6 @@ using AngsT = std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vecto
 using ObsT = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> >;
 using MatchesT = std::vector<std::pair<int, int> >; // tells us which maps overlap
 using BBsT = std::vector<Eigen::Matrix2d, Eigen::aligned_allocator<Eigen::Matrix2d> >;
-
-// NOTE: this seems reasonable
-struct mbes_ping
-{
-
-    unsigned int id_;
-    std::string time_string_;
-    long long time_stamp_;
-    double heading_;
-    double heave_;
-    double pitch_;
-    double roll_;
-    bool first_in_file_;
-    Eigen::Vector3d pos_; // NOTE: this comes from associating ping with nav data
-
-    std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > beams;
-
-	template <class Archive>
-    void serialize( Archive & ar )
-    {
-        ar(CEREAL_NVP(id_), CEREAL_NVP(time_string_), CEREAL_NVP(time_stamp_), CEREAL_NVP(heading_),
-		   CEREAL_NVP(heave_), CEREAL_NVP(pitch_), CEREAL_NVP(roll_), CEREAL_NVP(first_in_file_));
-    }
-
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-};
-
-struct nav_entry
-{
-
-    std::string time_string_;
-    long long time_stamp_;
-    bool first_in_file_;
-    Eigen::Vector3d pos_;
-	
-    template <class Archive>
-    void serialize( Archive & ar )
-    {
-        ar(CEREAL_NVP(time_string_), CEREAL_NVP(time_stamp_), CEREAL_NVP(first_in_file_));
-    }
-
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-};
 
 void match_timestamps(std::vector<mbes_ping>& pings, std::vector<nav_entry>& entries);
 void view_cloud(const std::vector<mbes_ping>& pings);
