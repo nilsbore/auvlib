5587d1d2714f54eb5139de250965ea98445719d2
Nils Bore <nilsbore@gmail.com>
2018-09-11 13:15:48 +0200

    Finished the reader example

--- example_projects/data_project/src/example_reader.cpp
+++ example_projects/data_project/src/example_reader.cpp
@@ -5,6 +5,7 @@
 #include <data_tools/csv_data.h>
 #include <data_tools/xtf_data.h>
 #include <data_tools/navi_data.h>
+#include <data_tools/all_data.h>
 #include <data_tools/transforms.h>
 
 #include <chrono>
@@ -15,12 +16,14 @@ int main(int argc, char** argv)
 {
     string folder_str;
     string file_str;
+    string type;
 
 	cxxopts::Options options("MyProgram", "One line description of MyProgram");
 	options.add_options()
       ("help", "Print help")
       ("folder", "Input folder containing mbes files", cxxopts::value(folder_str))
-      ("file", "Output file", cxxopts::value(file_str));
+      ("file", "Output file", cxxopts::value(file_str))
+      ("type", "Type of data to read, options: all, xtf, navi, gsf", cxxopts::value(type));
 
     auto result = options.parse(argc, argv);
 	if (result.count("help")) {
@@ -28,7 +31,11 @@ int main(int argc, char** argv)
         exit(0);
 	}
     if (result.count("folder") == 0) {
-		cout << "Please provide input swaths and sss args..." << endl;
+		cout << "Please provide folder containing mbes or sss files..." << endl;
+		exit(0);
+    }
+    if (result.count("type") == 0) {
+		cout << "Please provide input type, options: all, xtf, navi, gsf" << endl;
 		exit(0);
     }
     if (result.count("file") == 0) {
@@ -42,14 +49,41 @@ int main(int argc, char** argv)
 	cout << "Input mbes folder : " << folder << endl;
 	cout << "Output file : " << path << endl;
 
+    // if side scan, read and save, the return
+    if (type == "xtf") {
+        xtf_sss_ping::PingsT pings = parse_folder<xtf_sss_ping>(folder);
+        write_data(pings, path);
+        cout << "Read folder " << folder << " with type " << type << " and saved to file " << path << endl;
+        return 0;
+    }
+
+    // otherwise we have multibeam data, read and save
     mbes_ping::PingsT std_pings;
-    {
+    if (type == "gsf") {
         gsf_mbes_ping::PingsT pings = parse_folder<gsf_mbes_ping>(folder);
         std::stable_sort(pings.begin(), pings.end(), [](const gsf_mbes_ping& ping1, const gsf_mbes_ping& ping2) {
             return ping1.time_stamp_ < ping2.time_stamp_;
         });
         std_pings = convert_pings(pings);
     }
+    else if (type == "all") {
+	    all_nav_entry::EntriesT entries = parse_folder<all_nav_entry>(folder);
+        all_mbes_ping::PingsT pings = parse_folder<all_mbes_ping>(folder);
+        std_pings = convert_matched_entries(pings, entries);
+    }
+    else if (type == "navi") {
+        boost::filesystem::path nav_folder = folder.parent_path() / "NavUTM";
+        std_pings = parse_folder<mbes_ping>(folder);
+        nav_entry::EntriesT entries = parse_folder<nav_entry>(nav_folder);
+        match_timestamps(std_pings, entries);
+    }
+    else {
+        cout << "Type " << type << " is not supported!" << endl;
+        return 0;
+    }
+
+    write_data(std_pings, path);
+    cout << "Read folder " << folder << " with type conversion " << type << "-> mbes_ping and saved to file " << path << endl;
 
     return 0;
 }
