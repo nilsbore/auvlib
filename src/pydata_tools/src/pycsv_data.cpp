813d192193705a60e36920a82fc2f7c12f5a99db
Nils Bore <nilsbore@gmail.com>
2018-11-19 16:52:34 +0100

    Added nice docs for csv python

--- src/pydata_tools/src/pycsv_data.cpp
+++ src/pydata_tools/src/pycsv_data.cpp
@@ -7,27 +7,27 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(csv_data, m) {
-    m.doc() = "Basic utilities for reading csv based data"; // optional module docstring
+    m.doc() = "Basic utilities for working with csv based data"; // optional module docstring
 
-    py::class_<csv_nav_entry>(m, "csv_nav_entry")
+    py::class_<csv_nav_entry>(m, "csv_nav_entry", "Class for a csv based nav entry")
         .def(py::init<>())
-        .def_readwrite("time_string_", &csv_nav_entry::time_string_)
-        .def_readwrite("time_stamp_", &csv_nav_entry::time_stamp_)
-        .def_readwrite("roll_", &csv_nav_entry::roll_)
-        .def_readwrite("pitch_", &csv_nav_entry::pitch_)
-        .def_readwrite("heading_", &csv_nav_entry::heading_)
-        .def_readwrite("roll_std_", &csv_nav_entry::roll_std_)
-        .def_readwrite("pitch_std_", &csv_nav_entry::pitch_std_)
-        .def_readwrite("heading_std_", &csv_nav_entry::heading_std_)
-        .def_readwrite("lat_", &csv_nav_entry::lat_)
-        .def_readwrite("long_", &csv_nav_entry::long_)
-        .def_readwrite("altitude", &csv_nav_entry::altitude)
-        .def_readwrite("pos_", &csv_nav_entry::pos_)
-        .def_readwrite("vel_", &csv_nav_entry::pos_)
-        .def_static("parse_file", &parse_file_from_str<csv_nav_entry>)
-        .def_static("parse_folder", &parse_folder_from_str<csv_nav_entry>)
-        .def_static("read_data", &read_data_from_str<csv_nav_entry::EntriesT>);
+        .def_readwrite("time_string_", &csv_nav_entry::time_string_, "Member")
+        .def_readwrite("time_stamp_", &csv_nav_entry::time_stamp_, "Member")
+        .def_readwrite("roll_", &csv_nav_entry::roll_, "Member")
+        .def_readwrite("pitch_", &csv_nav_entry::pitch_, "Member")
+        .def_readwrite("heading_", &csv_nav_entry::heading_, "Member")
+        .def_readwrite("roll_std_", &csv_nav_entry::roll_std_, "Member")
+        .def_readwrite("pitch_std_", &csv_nav_entry::pitch_std_, "Member")
+        .def_readwrite("heading_std_", &csv_nav_entry::heading_std_, "Member")
+        .def_readwrite("lat_", &csv_nav_entry::lat_, "Member")
+        .def_readwrite("long_", &csv_nav_entry::long_, "Member")
+        .def_readwrite("altitude", &csv_nav_entry::altitude, "Member")
+        .def_readwrite("pos_", &csv_nav_entry::pos_, "Member")
+        .def_readwrite("vel_", &csv_nav_entry::pos_, "Member")
+        .def_static("parse_file", &parse_file_from_str<csv_nav_entry>, "Parse csv_nav_entry from .csv file")
+        .def_static("parse_folder", &parse_folder_from_str<csv_nav_entry>, "Parse csv_nav_entry from folder of .csv files")
+        .def_static("read_data", &read_data_from_str<csv_nav_entry::EntriesT>, "Read csv_nav_entry::EntriesT from .cereal file");
 
-    m.def("write_data", &write_data_from_str<csv_nav_entry::EntriesT>, "A function which writes csv nav entries to a .cereal file");
-    m.def("convert_matched_entries", (xtf_sss_ping::PingsT (*)(xtf_sss_ping::PingsT&, csv_nav_entry::EntriesT&) ) &convert_matched_entries, "A function which matches xtf pings and csv nav entries");
+    m.def("write_data", &write_data_from_str<csv_nav_entry::EntriesT>, "Write csv_nav_entry::EntriesT to .cereal file");
+    m.def("convert_matched_entries", (xtf_sss_ping::PingsT (*)(xtf_sss_ping::PingsT&, csv_nav_entry::EntriesT&) ) &convert_matched_entries, "Match xtf_sss_ping::PingsT and csv_nav_entry::EntriesT and assign pos info to pings");
 }
