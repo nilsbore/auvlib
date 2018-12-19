da728cf9d785c989b053bbe4333391bdce2c466d
Nils Bore <nilsbore@gmail.com>
2018-12-19 12:25:37 +0100

    Added script for plotting sidescan waterfall together with bathy

---
+++ src/pybathy_maps/examples/plot_sss_waterfalls.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+from pybathy_maps import patch_draper, draping_image, mesh_map
+from pydata_tools import data_structures, gsf_data
+from numpy import sign
+import numpy as np
+from copy import deepcopy
+import sys
+import os
+
+def plot_waterfalls(map_image):
+    
+    f, axarr = plt.subplots(1, 2)
+
+    axarr[0].imshow(map_image.sss_waterfall_image, cmap=plt.gray(), interpolation='none')
+    axarr[1].imshow(map_image.sss_waterfall_depth, cmap=plt.jet(), interpolation='none')
+
+    plt.show()
+
+map_images = draping_image.sss_map_image.read_data("map_images_cache.cereal")
+
+for map_image in map_images:
+    plot_waterfalls(map_image)
