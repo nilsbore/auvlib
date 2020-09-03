55e9ff73402db21e09178f10dba9f35b9b17a2af
xyp8023 <xyp8023@gmail.com>
2020-09-03 14:14:57 +0200

    added directly reading roll and pitch from .xtf files

--- src/data_tools/src/xtf_data.cpp
+++ src/data_tools/src/xtf_data.cpp
@@ -125,6 +125,8 @@ xtf_sss_ping process_side_scan_ping(XTFPINGHEADER *PingHeader, XTFFILEHEADER *XT
    ping.pos_ = Eigen::Vector3d(easting, northing, -PingHeader->SensorDepth);
    ping.heading_ = M_PI/180.*PingHeader->SensorHeading;
    ping.heading_ = 0.5*M_PI-ping.heading_; // TODO: need to keep this for old data
+   ping.roll_ = M_PI/180.*PingHeader->SensorRoll;
+   ping.pitch_ =  M_PI/180.*PingHeader->SensorPitch;
    ping.sound_vel_ = PingHeader->SoundVelocity;
 
    boost::posix_time::ptime data_time(boost::gregorian::date(PingHeader->Year, PingHeader->Month, PingHeader->Day), boost::posix_time::hours(PingHeader->Hour)+boost::posix_time::minutes(PingHeader->Minute)+boost::posix_time::seconds(PingHeader->Second)+boost::posix_time::milliseconds(10*int(PingHeader->HSeconds))); 
