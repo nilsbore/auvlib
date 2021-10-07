e5bec25d5833b69a9937eeb33baf82f5f8aa5f4e
Li Ling <liling@kth.se>
2021-10-07 16:46:07 +0200

    Add sensor offset class structure for parsing sensor offset files

---
+++ src/pydata_tools/src/pysensor_offset.cpp
@@ -0,0 +1,32 @@
+/* Copyright 2021 Li Ling (liling@kth.se)
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <data_tools/sensor_offset.h>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+
+using namespace sensor_offset;
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(sensor_offset, m) {
+    m.doc() = "Module for parsing sensor offset files, e.g. arms.init file from Kongsberg Hugin AUV";
+
+    py::class_<pos_offset>(m, "pos_offset", "Class for positional sensor offset")
+        .def(py::init<>())
+        .def_readwrite("x", &pos_offset::x, "Positional offset in X-axis")
+        .def_readwrite("y", &pos_offset::x, "Positional offset in Y-axis")
+        .def_readwrite("z", &pos_offset::x, "Positional offset in Z-axis");
+
+    m.def("parse_offset_file", &parse_offset_file, "Parse sensor offset file. Note that the following sign convention is assumed: +x=forward, +y=starboard, +z=down");
+}
