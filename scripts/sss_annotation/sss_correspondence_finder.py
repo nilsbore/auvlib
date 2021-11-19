8be23eecb9eaa295a3eb0618939855ee0ba8e959
Li Ling <liling@kth.se>
2021-11-19 19:59:01 +0100

    Add methods to plot annotated keypoints and correspondences

--- scripts/sss_annotation/sss_correspondence_finder.py
+++ scripts/sss_annotation/sss_correspondence_finder.py
@@ -4,9 +4,11 @@ from collections import OrderedDict
 import matplotlib.pyplot as plt
 import numpy as np
 from auvlib.bathy_maps.map_draper import sss_meas_data
+from skimage.feature import plot_matches
 
 from sss_annotations_manager import SSSAnnotationManager
-from sss_plot import SSSAnnotationPlot
+from sss_plot import SSSAnnotationPlot, SSSPlotData
+from utils import normalize_waterfall_image
 
 
 class SSSDrapingFolderParser():
@@ -108,7 +110,7 @@ class SSSDrapingFolderParser():
             raise ValueError(
                 f'Valid index range: (0, {self.num_data_files - 1})')
 
-        target_filename = list(self.data_files.keys())[idx]
+        target_filename = self._idx_to_filename(idx)
         target_filepath = self.data_files[target_filename]
         # overlapping_filepaths = [target_filename]
         overlapping_filenames = self.get_overlapping_filenames(target_filename)
@@ -125,3 +127,64 @@ class SSSDrapingFolderParser():
                                                    annotation_plot.annotations)
         self.annotations_manager.write_annotations()
         return self.annotations_manager.correspondence_annotations
+
+    def _idx_to_filename(self, idx):
+        return list(self.data_files.keys())[idx]
+
+    def plot_keypoints_for_all_files(self):
+        """Plot annotated keypoints for all files in self.folder"""
+        for i in range(len(self.data_files)):
+            self.plot_keypoints(i)
+        plt.show()
+
+    def plot_keypoints(self, idx, figsize=(5, 10), normalize_image=True):
+        """Plot annotated keypoints for file number idx. Call plt.show() to
+        bring up the image."""
+        filename = self._idx_to_filename(idx)
+        plot = SSSPlotData(self.data_files[filename], figsize, normalize_image)
+        kps = self.annotations_manager.keypoints[filename]
+        ping_nbrs, col_nbrs = [], []
+        for (ping_nbr, col_nbr) in kps.keys():
+            ping_nbrs.append(ping_nbr)
+            col_nbrs.append(col_nbr)
+        plot.ax.scatter(col_nbrs, ping_nbrs, c='yellow', s=5)
+
+    def plot_correspondences(self, idx1, idx2):
+        """Plot annotated correspondences for file number (idx1, idx2). Call
+        plt.show() to bring up the image."""
+        filename1 = self._idx_to_filename(idx1)
+        filename2 = self._idx_to_filename(idx2)
+        matched_kps1 = []
+        matched_kps2 = []
+        no_match_kp1, no_match_kp2 = [], []
+        for kp_id, kp_dict in self.annotations_manager.correspondence_annotations.items(
+        ):
+            if filename1 in kp_dict and filename2 in kp_dict:
+                matched_kps1.append(list(kp_dict[filename1]))
+                matched_kps2.append(list(kp_dict[filename2]))
+            elif filename1 in kp_dict:
+                no_match_kp1.append(list(kp_dict[filename1]))
+            elif filename2 in kp_dict:
+                no_match_kp2.append(list(kp_dict[filename2]))
+
+        matches = np.array([[i, i] for i in range(len(matched_kps1))])
+        matched_kps1.extend(no_match_kp1)
+        kps1 = np.array(matched_kps1)
+        matched_kps2.extend(no_match_kp2)
+        kps2 = np.array(matched_kps2)
+
+        fig, ax = plt.subplots(1, 1)
+        img1 = normalize_waterfall_image(
+            sss_meas_data.read_single(
+                self.data_files[filename1]).sss_waterfall_image)
+        img2 = normalize_waterfall_image(
+            sss_meas_data.read_single(
+                self.data_files[filename1]).sss_waterfall_image)
+        plot_matches(ax,
+                     img1,
+                     img2,
+                     kps1,
+                     kps2,
+                     matches,
+                     keypoints_color='y',
+                     matches_color='g')
